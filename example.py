9adcb2957279bb54c1356c81c5eedc63e6771b7f
Akhil Kumar Gupta <akgupta1337@gmail.com>
2024-02-09 18:15:26 +0530

    now you can add files in recorded_audios with any name

--- example.py
+++ example.py
@@ -1,10 +1,29 @@
 from logic.Fingerprint import Fingerprint
+import os
 
-file_name = ["recorded_audios/recorded_classical.wav","recorded_audios/recorded_jazz.wav","recorded_audios/recorded_pop.wav"]
+# Define the directory path
+directory = "recorded_audios"
+
+# Initialize an empty list to store song names with relative paths
+song_list = []
+
+# Iterate over all files in the directory
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Check if the file has a .wav extension
+        if file.endswith(".wav"):
+            # Get the relative path of the file
+            relative_path = os.path.relpath(os.path.join(root, file), directory)
+            # Add the relative path and file name to the list
+            song_list.append(f"recorded_audios/{relative_path}")
+
+# Print the list of song names with relative paths
+
+song_list = ["recorded_audios/recorded_classical.wav","recorded_audios/recorded_jazz.wav","recorded_audios/recorded_pop.wav"]
 
 engine = Fingerprint()
 
-for song_name in file_name:
+for song_name in song_list:
     print("----------------------------------")
     result = engine.shazzam(song_name)
     song_name = song_name.split("/")[-1]
