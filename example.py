7d76271f6d29391bbfced409542e2bfb8aa005f2
Akhil Kumar Gupta <akgupta1337@gmail.com>
2024-02-09 18:16:11 +0530

    fixed example.py

--- example.py
+++ example.py
@@ -10,14 +10,10 @@ song_list = []
 # Iterate over all files in the directory
 for root, dirs, files in os.walk(directory):
     for file in files:
-        # Check if the file has a .wav extension
         if file.endswith(".wav"):
-            # Get the relative path of the file
             relative_path = os.path.relpath(os.path.join(root, file), directory)
-            # Add the relative path and file name to the list
             song_list.append(f"recorded_audios/{relative_path}")
 
-# Print the list of song names with relative paths
 
 song_list = ["recorded_audios/recorded_classical.wav","recorded_audios/recorded_jazz.wav","recorded_audios/recorded_pop.wav"]
 
