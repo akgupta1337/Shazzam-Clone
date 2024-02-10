6390d937e42581601b93b4c89db0588d96f57845
Akhil Kumar Gupta <akgupta1337@gmail.com>
2024-02-10 21:02:57 +0530

    edited files

--- logic/Fingerprint.py
+++ logic/Fingerprint.py
@@ -99,18 +99,6 @@ class Fingerprint:
         Fs = self.DEFAULT_FS
         fan_value = self.DEFAULT_FAN_VALUE
         amp_min = self.DEFAULT_AMP_MIN
-        """
-        FFT the channel, log transform output, find local maxima, then return locally sensitive hashes.
-
-        :param channel_samples: channel samples to fingerprint.
-        :param Fs: audio sampling rate.
-        :param wsize: FFT windows size.
-        :param wratio: ratio by which each sequential window overlaps the last and the next window.
-        :param fan_value: degree to which a fingerprint can be paired with its neighbors.
-        :param amp_min: minimum amplitude in spectrogram in order to be considered a peak.
-        :return: a list of hashes with their corresponding offsets.
-        """
-        # FFT the signal and extract frequency components
         arr2D = mlab.specgram(
             channel_samples,
             NFFT=self.wsize,
@@ -118,94 +106,44 @@ class Fingerprint:
             window=mlab.window_hanning,
             noverlap=int(self.wsize * self.wratio))[0]
 
-        # Apply log transform since specgram function returns linear array. 0s are excluded to avoid np warning.
         arr2D = 10 * np.log10(arr2D, out=np.zeros_like(arr2D), where=(arr2D != 0))
 
         local_maxima = self.get_2D_peaks(arr2D)
-        # return hashes
 
         return self.generate_hashes(local_maxima)
     
     
     def get_2D_peaks(self,arr2D):
         amp_min = self.DEFAULT_AMP_MIN
-        """
-        Extract maximum peaks from the spectogram matrix (arr2D).
-
-        :param arr2D: matrix representing the spectogram.
-        :param plot: for plotting the results.
-        :param amp_min: minimum amplitude in spectrogram in order to be considered a peak.
-        :return: a list composed by a list of frequencies and times.
-        """
-        # Original code from the repo is using a morphology mask that does not consider diagonal elements
-        # as neighbors (basically a diamond figure) and then applies a dilation over it, so what I'm proposing
-        # is to change from the current diamond figure to a just a normal square one:
-        #       F   T   F           T   T   T
-        #       T   T   T   ==>     T   T   T
-        #       F   T   F           T   T   T
-        # In my local tests time performance of the square mask was ~3 times faster
-        # respect to the diamond one, without hurting accuracy of the predictions.
-        # I've made now the mask shape configurable in order to allow both ways of find maximum peaks.
-        # That being said, we generate the mask by using the following function
-        # https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.generate_binary_structure.html
+       
         struct = generate_binary_structure(2, self.CONNECTIVITY_MASK)
 
-        #  And then we apply dilation using the following function
-        #  http://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.iterate_structure.html
-        #  Take into account that if PEAK_NEIGHBORHOOD_SIZE is 2 you can avoid the use of the scipy functions and just
-        #  change it by the following code:
-        #  neighborhood = np.ones((PEAK_NEIGHBORHOOD_SIZE * 2 + 1, PEAK_NEIGHBORHOOD_SIZE * 2 + 1), dtype=bool)
         neighborhood = iterate_structure(struct, self.PEAK_NEIGHBORHOOD_SIZE)
 
-        # find local maxima using our filter mask
         local_max = maximum_filter(arr2D, footprint=neighborhood) == arr2D
 
-        # Applying erosion, the dejavu documentation does not talk about this step.
         background = (arr2D == 0)
         eroded_background = binary_erosion(background, structure=neighborhood, border_value=1)
 
-        # Boolean mask of arr2D with True at peaks (applying XOR on both matrices).
         detected_peaks = local_max != eroded_background
 
-        # extract peaks
         amps = arr2D[detected_peaks]
         freqs, times = np.where(detected_peaks)
 
-        # filter peaks
         amps = amps.flatten()
 
-        # get indices for frequency and time
         filter_idxs = np.where(amps > amp_min)
 
         freqs_filter = freqs[filter_idxs]
         times_filter = times[filter_idxs]
 
-            # scatter of the peaks
-        # fig, ax = plt.subplots()
-        # ax.imshow(arr2D)
-        # ax.scatter(times_filter, freqs_filter)
-        # ax.set_xlabel('Time')
-        # ax.set_ylabel('Frequency')
-        # ax.set_title("Spectrogram")
-        # plt.gca().invert_yaxis()
-        # plt.show()
         
         return list(zip(freqs_filter, times_filter))
     
     def generate_hashes(self,peaks):
         fan_value: int = self.DEFAULT_FAN_VALUE
-        """
-        Hash list structure:
-        sha1_hash[0:FINGERPRINT_REDUCTION]    time_offset
-            [(e05b341a9b77a51fd26, 32), ... ]
 
-        :param peaks: list of peak frequencies and times.
-        :param fan_value: degree to which a fingerprint can be paired with its neighbors.
-        :return: a list of hashes with their corresponding offsets.
-        """
-        # frequencies are in the first position of the tuples
         idx_freq = 0
-        # times are in the second position of the tuples
         idx_time = 1
 
         if self.PEAK_SORT:
