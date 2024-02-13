3cbf568c89b7a82235ee849009a5bee8eb701f73
Akhil Kumar Gupta <akgupta1337@gmail.com>
2024-02-13 20:04:38 +0530

    edited code

--- EXAMPLE.py
+++ EXAMPLE.py
@@ -3,6 +3,15 @@ from logic.Shazzam import Shazzam
 engine = Shazzam()
 
 
-
+'''
+this will add songs present in 'mp3' folder to database
+run it only once to load hashes in database, after that comment it out 
+and only run match_song()
+'''
 engine.add_songs()
+
+'''
+just write song name, which is present in 'test' folder 
+no need to mention relative path
+'''
 engine.match_song('sean_secs.wav')
